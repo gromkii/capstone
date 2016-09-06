87ac0a1d08ac1c28da808ef73d8ff79100034ff3
Dax <limeside@gmail.com>
2016-09-06 10:09:09 -0500

    Add methods to index controller, need to actually write. :rabbit:

--- public/javascript/controllers/index.js
+++ public/javascript/controllers/index.js
@@ -5,6 +5,8 @@
     .controller('IndexController', IndexController);
 
   routeConfig.$inject = ['$routeProvider', '$locationProvider'];
+  registerPost.$inject = ['$http'];
+  signinPost.$inject = ['$http'];
 
   function routeConfig($routeProvider, $locationProvider){
     $routeProvider
@@ -25,14 +27,24 @@
 
     vm.greeting = 'It works btw.';
     vm.registerModal = registerModal;
+    vm.registerPost = registerPost;
     vm.signinModal = signinModal;
+    vm.signinPost = signinPost;
   }
 
   function registerModal(){
     // Do shit.
   }
 
+  function registerPost($http){
+    // $http.post to API when form is valid.
+  }
+
   function signinModal(){
     // Do shit.
+  }
+
+  function signinPost($http){
+
   }
 })();
