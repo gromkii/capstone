04b7b91025dfc5df9cbc91fcf1c59b2410414459
Dax <limeside@gmail.com>
2016-09-06 15:40:40 -0500

    Add passport, not tested. :rabbit:

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 var express = require('express'),
-  router = express.Router();
+  router = express.Router(),
+  passport = require('passport');
 
+router.route('/login')
+  .post(passport.authenticate('local', {
+    failureRedirect: '/auth/login'
+  }), (req, res) => {
+    res.send('You did it.');
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
+var express        = require('express'),
+    app            = express(),
+    bodyParser     = require('body-parser'),
     methodOverride = require('method-override'),
-    api = require('./routes/api.js'),
-    auth = require('./routes/auth.js');
+    cookieSession  = require('cookie-session'),
+    passport       = require('passport'),
+  	LocalStrategy  = require('passport-local').Strategy,
+    api            = require('./routes/api.js'),
+    auth           = require('./routes/auth.js');
 
 // --- Middleware --- //
 app.use(bodyParser.json())
@@ -13,10 +16,52 @@ app.use(bodyParser.json())
   .use('/bower_components', express.static(__dirname + '/bower_components'));
 
 
+// --- Passport --- //
+app.use(cookieSession({
+	name: 'session',
+	keys: [process.env.KEY1, process.env.KEY2]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+	usernameField: 'username',
+	passwordField: 'password',
+	session: false
+}, (username, password, done) => {
+	// Check id of user, retrieve row in users table.
+	Users().where('username', username)
+		.first()
+		.then( user => {
+			// compareSync the user's hashed password.
+			if (user && bcrypt.compareSync(password, user.password)){
+				// On match, return confirmation of session.
+				return done(null, user);
+			}
+			// Otherwise, return no session, redirect.
+			return done(null, false);
+		})
+}));
+
+passport.serializeUser(function(user, done) {
+	console.log(user);
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Users()
+		.where('id', id)
+		.first()
+		.then( user => {
+
+			done(null, user);
+	});
+});
+
 // --- Routing --- //
 app.use('/api', api);
 app.use('/auth', auth);
- 
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/views/index.html')
 });
