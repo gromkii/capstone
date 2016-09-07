f9a22946b69935c95db27442b0f0a8a158a61380
Dax <limeside@gmail.com>
2016-09-07 13:26:53 -0500

    Add user Dashboard, session schema and seeds. :rabbit:

--- seeds/users.js
+++ seeds/users.js
@@ -72,5 +72,19 @@ exports.seed = function(knex, Promise) {
             })
           ]);
         });
+    }).then(() => {
+      return knex('sessions').del()
+        .then(()=> {
+          return Promise.all([
+            knex('sessions').insert({
+              session_name:'Fill Murray Hack n Slash',
+              game_name:'Dungeons and Dragons',
+              session_desc:'Fill Murray doin shit.',
+              header_url:'http://fillmurray.com/200/600',
+              start_date:'2016-09-07T18:25:49+00:00',
+              runtime:'3 Hours'
+            })
+          ])
+        })
     });
 };
