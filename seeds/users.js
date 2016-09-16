92fb4429b0e4c915fa1282c35875f4a7b2ad63a0
Dax <limeside@gmail.com>
2016-09-15 22:17:17 -0500

    Add more seed data, might fuck up tests? :rabbit:

--- seeds/users.js
+++ seeds/users.js
@@ -83,8 +83,32 @@ exports.seed = function(knex, Promise) {
               header_url:'http://fillmurray.com/800/200',
               start_date:'2016-09-07T18:25:49+00:00',
               runtime:'3 Hours',
-              skill_level:1
-            })
+              skill_level:1,
+              host_id:1,
+              num_players:6
+            }),
+            knex('sessions').insert({
+              session_name:'Place Quest',
+              game_name:'Pathfinder',
+              session_desc:'The other day the grass was brown, now it’s green because I ain’t give up. Never surrender. You do know, you do know that they don’t want you to have lunch. I’m keeping it real with you, so what you going do is have lunch. Another one. Special cloth alert. Special cloth alert. I’m up to something.',
+              header_url:'http://placecage.com/800/200',
+              start_date:'2016-09-07T18:25:49+00:00',
+              runtime:'40 minutes',
+              skill_level:3,
+              host_id:2,
+              num_players:3
+            }),
+            knex('sessions').insert({
+              session_name:'Steven Segall\'s Actual Life',
+              game_name:'Legend of the Five Rings',
+              session_desc:'The other day the grass was brown, now it’s green because I ain’t give up. Never surrender. You do know, you do know that they don’t want you to have lunch. I’m keeping it real with you, so what you going do is have lunch. Another one. Special cloth alert. Special cloth alert. I’m up to something.',
+              header_url:'http://stevensegallery.com/800/200',
+              start_date:'2016-09-07T18:25:49+00:00',
+              runtime:'An Entire Lifetime',
+              skill_level:2,
+              host_id:3,
+              num_players:2
+            }),
           ])
         })
     }).then(() => {
@@ -100,8 +124,20 @@ exports.seed = function(knex, Promise) {
               session_id:1
             }),
             knex('user_sessions').insert({
+              user_id:2,
+              session_id:2
+            }),
+            knex('user_sessions').insert({
+              user_id:3,
+              session_id:3
+            }),
+            knex('user_sessions').insert({
               user_id:3,
               session_id:1
+            }),
+            knex('user_sessions').insert({
+              user_id:3,
+              session_id:3
             })
           ])
         })
