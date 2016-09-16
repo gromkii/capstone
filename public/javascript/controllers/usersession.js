4b1cdc8785145a73266fc8b2bc84da1db01e6d0a
Dax <limeside@gmail.com>
2016-09-15 21:57:16 -0500

    Add list of users current running sessions. :rabbit:

--- public/javascript/controllers/usersession.js
+++ public/javascript/controllers/usersession.js
@@ -1,11 +1,11 @@
 (function(){
   angular
-    .module('userSession', [])
+    .module('userSession', ['Sessions'])
     .config(routeConfig)
     .controller('UserSessionController', UserSessionController)
 
   routeConfig.$inject = ['$routeProvider', '$locationProvider']
-
+  UserSessionController.$inject = ['$http', 'Sessions']
 
 
   function routeConfig($routeProvider, $locationProvider) {
@@ -22,8 +22,22 @@
     });
   }
 
-  function UserSessionController(){
+  function UserSessionController($http, Sessions){
     var vm = this;
     vm.greeting = "Hello bitches.";
+
+    $http.get('/auth/user')
+      .then( results => {
+        var u = results.data;
+        if (u.id) {
+          $http.get(`/api/user/${u.id}/sessions`)
+            .then( results => {
+
+              vm.mySessions = results.data.sessions
+
+              console.log(vm.mySessions);
+            })
+        }
+      });
   }
 })()
